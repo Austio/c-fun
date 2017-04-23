ba5ef8c27005b1de127b2a926ab273bce00ade03
Austin <lonnieastory@gmail.com>
2017-04-23 13:50:21 -0500

    Adding pointer reassignment ofr str2 to str1 to demonstrate that str2 is just a reference pointer

--- array.c
+++ array.c
@@ -4,6 +4,7 @@
 int main(int argc, char **argv) {
   char str1[] = "Hiya";
   char *str2 = "Howdy";
+  str2 = &str1;
 
   printf("%d %d %s\n", &str1, str1, str1);
   printf("%d %d %s\n", &str2, str2, str2);
